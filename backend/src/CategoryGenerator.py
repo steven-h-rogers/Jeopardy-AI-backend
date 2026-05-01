9efc7f7c9ca87298012403ce5733f00c21dd3fba
Steven Rogers <s.hunter.rogers@gmail.com>
2026-05-01 17:35:53 -0400

    added non-agentic method

--- backend/src/CategoryGenerator.py
+++ backend/src/CategoryGenerator.py
@@ -30,7 +30,8 @@ class CategoryGenerator:
     def __init__(self):
         self.categories = []
 
-        llm = ChatOpenAI(model="gpt-5-nano", temperature=0)
+        self._llm = ChatOpenAI(model="gpt-5-nano", temperature=0)
+        self.structured_llm = self._llm.with_structured_output(CategoryResponse)
 
 
         self.model = ChatOpenAI(
@@ -44,10 +45,17 @@ class CategoryGenerator:
     def generate_categories(self, num_categories):
         # ? consider using t strings as they are better at preventing injection type attacks
         return self.categoryAgent.invoke({'messages': [{'role': 'system', 'content': f'generate {num_categories} jeopardy-style categories' }]})
+    
+    def generate_categories_updated(self, num_categories):
+        prompt = f'Generate exactly {num_categories} unique Jeopardy-style categories that can either be original or from the show'
+        return self.structured_llm.invoke(prompt)
 
 #  *For Testing Purposes Only
-# //num_categories = 6
-# //categoryGenerator = CategoryGenerator()
-# //response = categoryGenerator.generate_categories(num_categories)
-# //print(response)
-# //print(response['structured_response'].categories)
+num_categories = 6
+categoryGenerator = CategoryGenerator()
+
+response1 = categoryGenerator.generate_categories_updated(6)
+print(response1.categories)
+
+response2 = categoryGenerator.generate_categories(num_categories)
+print(response2.categories)
