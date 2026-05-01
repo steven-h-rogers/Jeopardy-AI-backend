c6468870886cfbd8ed0bf4b52e1c07d7e4c8d72c
Steven Rogers <s.hunter.rogers@gmail.com>
2026-05-01 19:29:41 -0400

    added timer decorator

--- backend/src/CategoryGenerator.py
+++ backend/src/CategoryGenerator.py
@@ -1,3 +1,6 @@
+import time
+import functools
+
 from dotenv import load_dotenv
 
 from pydantic import BaseModel, Field
@@ -9,6 +12,22 @@ from langchain.agents.structured_output import ToolStrategy
 
 
 load_dotenv()
+
+
+def timer(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+
+        duration = end_time - start_time
+        print(f"Executed {func.__name__} in {duration:.4f} seconds")
+        return result
+    return wrapper
+
+
+
 
 
 
