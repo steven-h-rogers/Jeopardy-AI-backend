03ca20368b2dab07ebdb9d314017060c9676ac89
steven-h-rogers <s.hunter.rogers@gmail.com>
2026-05-13 16:39:17 -0400

    restructured game-container into table, added mockup for question

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,3 +1,6 @@
 import * as constants from "./constants";
 
+// game state should be stored in a JSON primarily so that the game board can be re-rendered after 
+// every question, but also so that a user could come back to their game later if they wanted
+
 
