df8fef85075b23b2de5d9c5c976f4d58cc98e4a0
Steven Rogers <s.hunter.rogers@gmail.com>
2026-06-01 12:23:08 -0400

    added basic savemanager methods

---
+++ frontend/scripts/game scripts/SaveManager.js
@@ -0,0 +1,15 @@
+class SaveManager{
+    constructor(){
+        this.game;
+        this.jsonState;
+    }
+
+    convertToJSON(game){
+        return JSON.stringify(game_obj);
+    }
+
+    convertToObject(game_string){
+        return JSON.parse(game_string);
+    }
+
+}
