f22171d021e5b9eb7790c6e1125d0a16aa4a50d7
steven-h-rogers <s.hunter.rogers@gmail.com>
2026-06-04 20:12:09 -0400

    renderGameBoard is now functional

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -12,6 +12,8 @@ const saveManager = new SaveManager();
 const game = new Game();
 saveManager.setGame(game);
 
+// default initialization to prompt the user to select a game mode
+// currently, the only allowed game mode is classic quickplay
 const quickplayButton = document.querySelector("#quickplay-classic");
 quickplayButton.addEventListener('click',()=>{
 setGameQuickplayScreen();
@@ -19,8 +21,7 @@ setGameQuickplayScreen();
 console.log(quickplayButton);
 
 
-
-//used to set the everything-container to the game selection screen
+// used to set the everything-container to the game selection screen
 function setGameSelectionScreen(){
     const everythingContainer = document.querySelector("#everything-container");
     everythingContainer.replaceChildren();
@@ -36,6 +37,7 @@ function setGameSelectionScreen(){
 }
 
 
+// set the current gameboard to a new classic game
 function setGameQuickplayScreen(){
     game.initializeGame(getClassicCategories(6), 1);
     const everythingContainer = document.querySelector("#everything-container");
@@ -44,34 +46,39 @@ function setGameQuickplayScreen(){
 
 }
 
-//TODO: this function needs to be fixed.
+// render the current loaded game board
 function renderGameBoard(){
-    console.log('game rendered');
     const everythingContainer = document.querySelector("#everything-container");
     const gameTable = document.createElement("table");
     gameTable.id = "game-table";
 
+    let rowIndex = 0;
     let table = game.table;
-    for (let i=0;i<table.columns.length;i++){ //col index
-        for(let j=0;j<table.columns[0].questionList.length;j++){ //row index
-            const tableRow = document.createElement("tr");
-            if (i===0){
-                const header = document.createElement("th");
-                header.textContent = table.columns[i].category;
-                header.id = `col${i}-category`;
-                tableRow.append(header);
-            }
-            else{
-                const tableData = document.createElement("td");
-                tableData.textContent = table.columns[i].questionList[j].displayValue;
-                tableData.id = `col${i}-q${j}`;
-                tableData.class = "question-tile";
-                tableRow.append(tableData);
-            }
-            gameTable.append(tableRow);
+    let numRows = table.columns[0].questionList.length; // TODO: there's definitely a cleaner way to do this
+
+    while (rowIndex<=numRows){
+        const tableRow = document.createElement("tr");
+        for (let i=0;i<table.columns.length;i++){ //col index
+                if (rowIndex===0){
+                    const header = document.createElement("th");
+                    header.textContent = table.columns[i].category;
+                    header.id = `col${i}-category`;
+                    tableRow.append(header);
+                }
+                else{
+                    const tableData = document.createElement("td");
+                    tableData.textContent = table.columns[i].questionList[rowIndex-1].displayValue;
+                    tableData.id = `col${i}-q${rowIndex}`;
+                    tableData.class = "question-tile";
+                    tableRow.append(tableData);
+                } 
         }
+        gameTable.append(tableRow);
+        rowIndex++;
     }
     everythingContainer.append(gameTable);
 }
+
+
 
 
