3d8244d785eed8ee71869078679ba31bdf6e3e83
steven-h-rogers <s.hunter.rogers@gmail.com>
2026-06-08 20:24:21 -0400

    hooked up judgment, summary, and topic extracter api endpoints to apimanager

--- frontend/scripts/APIManager.js
+++ frontend/scripts/APIManager.js
@@ -54,16 +54,83 @@ async function getQuestionAndAnswer(questionPayload){
      
 }
 
-async function getQuestionSummary(questionText){
-    //! DUMMY FUNCTION FOR TESTING
-    //TODO: Hook this up to API endpoint
+async function getJudgement(judgePayload){
+    try {
+        const response = await fetch(`${baseURL}/judge/judge-answer/`,{
+            method: 'POST', 
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(judgePayload)
+        });
 
-    const response = await fetch(`${baseURL}`)
+        if (!response.ok) {
+            throw new Error(`Server responded with status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('Judgment Generation Failed: ', error);
+    }
+}
+
+async function getQuestionSummary(questionText){
+    const summaryPayload = {question: questionText};
+
+    try{
+        const response = await fetch(`${baseURL}/summarizer/summarize-question/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(summaryPayload)
+        });
+
+        if (!response.ok){
+            throw new Error(`Server responded with status ${response.status}`);
+        }
+            const data = await response.json();
+            console.log(data);
+            return data;
+        
+    } catch (error) {
+        console.error('Summary Generation Failed: ', error);
+    }
 
 }
 
-getClassicCategories(7);
+async function getTopicExtraction(questionText){
+    const topicPayload = {question: questionText};
+
+    try {
+        const response = await fetch(`${baseURL}/summarizer/extract-topic/`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(topicPayload)
+        });
+
+        if (!response.ok){
+            throw new Error(`Server responded with status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('Topic Extraction Failed: ', error);
+    }
+}
+
+getQuestionSummary('Born in the tiny Vermont village of Plymouth Notch on July 4, 1872, he is the only U.S. president born on Independence Day.');
+getTopicExtraction('Born in the tiny Vermont village of Plymouth Notch on July 4, 1872, he is the only U.S. president born on Independence Day.');
+
+// ! fix judgement prompts so that incorrect answers are marked as so
+getJudgement({question: 'Born in the tiny Vermont village of Plymouth Notch on July 4, 1872, he is the only U.S. president born on Independence Day.', answer: 'Who is Calvin Coolidge', provided_answer: 'Who is Calvin Coolidge'});
+getJudgement({question: 'Born in the tiny Vermont village of Plymouth Notch on July 4, 1872, he is the only U.S. president born on Independence Day.', answer: 'Who is Teddy Roosevelt', provided_answer: 'Who is Calvin Coolidge'});
+getJudgement({question: 'Born in the tiny Vermont village of Plymouth Notch on July 4, 1872, he is the only U.S. president born on Independence Day.', answer: 'Calvin Coolidge', provided_answer: 'Who is Calvin Coolidge'});
 
 
-export {getClassicCategories, getQuestionAndAnswer, getQuestionSummary}
+export {getClassicCategories, getQuestionAndAnswer, getQuestionSummary, getJudgement, getTopicExtraction}
 
